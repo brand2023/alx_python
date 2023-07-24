4b8ddd2a71ce06b2716ec67e1e900f48ea77317b
brand2023 <brandonestephane@gmail.com>
2023-07-25 00:33:03 +0200

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
 word="Holberton"
-word_first_3=word[:3]
-word_last_2=word[7:]
-middle_word=word[1:8]
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+word1="School"
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[7:]))
+print("Middle word: {}".format(word[1:8]))
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[4:]))
+print("Middle word: {}".format(word[1:5]))
