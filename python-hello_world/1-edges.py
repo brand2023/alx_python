f108a04643ab3de93ebaf61fc8548d4b795c6792
brand2023 <brandonestephane@gmail.com>
2023-07-25 21:24:58 +0200

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word="Holberton"
 n=len(word)
 word_first_3=word[:3]
-word_last_2=word[n-1:]
-middle_word=word[1:n]
+word_last_2=word[n-2:]
+middle_word=word[1:n-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
