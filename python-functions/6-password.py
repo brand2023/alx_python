2982ad445f29072c2df3e169e380db946970e5e8
brand2023 <brandonestephane@gmail.com>
2023-07-29 19:13:51 +0200

    Password Validation Function

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,2 +1,16 @@
 def validate_password(password):
-    
+    Sym =['$', '@', '#', '%']
+    space=[' ']
+    if len(password)<8:
+        return(False)
+    if not any(char.isdigit() for char in password):
+        return(False)
+    if not any(char.isupper() for char in password):
+        return(False)
+    if not any(char.islower() for char in password):
+        return(False)
+    if not any(char.isSym() for char in password):
+        return(False)
+    if any(char.isspace() for char in password):
+        return(False)
+    return(True)
