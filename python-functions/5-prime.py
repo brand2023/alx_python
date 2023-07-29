c8ea4c933e51bf3811d74c02659a211b485c3a65
brand2023 <brandonestephane@gmail.com>
2023-07-29 16:48:23 +0200

    Prime Number Function

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,6 @@
 def is_prime(number):
     if number>=1:
-        if number%1==0 and number%number==0:
+        if number%1==0 and number%number==0 and (number%2!=0 or number%3!=0):
             return(True)
     else:
         return(False)
