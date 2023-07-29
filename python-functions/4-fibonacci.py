c99eb181602ee25089a2b0e77da71429c2ab6fcf
brand2023 <brandonestephane@gmail.com>
2023-07-29 16:22:52 +0200

    Fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -5,7 +5,7 @@ def fibonacci_sequence(n):
     elif n==1:
         t=[0]
     else:
-        t=[0,1]
         for i in range(1, n-1):
+            t=[0,1]
             t.append(t[i-1]+t[i])
     return(t)
