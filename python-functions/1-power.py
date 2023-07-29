34f0e9eaec40a3bc074ce2563db5943c0348b462
brand2023 <brandonestephane@gmail.com>
2023-07-29 15:25:38 +0200

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,10 +1,3 @@
 #!/usr/bin/env python3
-pow = __import__('1-power').pow
 def pow(a, b):
     return(a ** b)
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
-  
