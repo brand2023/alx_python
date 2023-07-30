f34c1fbe3b6d2cfcca2b5bb2b2448d4b64ed6c7e
brand2023 <brandonestephane@gmail.com>
2023-07-30 22:41:29 +0200

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -8,4 +8,5 @@ def safe_print_division(a, b):
         result="None"
         print("Denominator cannot be 0.")
     finally:
-        print("Inside result: {}".format(result),"{:d} / {:d} = {}".format(a, b, result))
+        print("Inside result: ",result)
+        print("{:d} / {:d} = {}".format(a, b, result))
