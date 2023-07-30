63d6c7f0c3de8625fab0fd77ff0bfeb48068960b
brand2023 <brandonestephane@gmail.com>
2023-07-30 22:45:44 +0200

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,11 +2,10 @@ def safe_print_division(a, b):
     try:
         result=a/b
     except TypeError:
-        result="None"
         print("a and b are integers")
     except ZeroDivisionError:
         result="None"
         print("Denominator cannot be 0.")
     finally:
-        print("Inside result: ",result)
+        print("Inside result:",result)
         print("{:d} / {:d} = {}".format(a, b, result))
