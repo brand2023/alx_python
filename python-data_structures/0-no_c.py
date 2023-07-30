9aa3e806233ee4cbbd2d6891a74e6cff89264432
brand2023 <brandonestephane@gmail.com>
2023-07-31 00:17:14 +0200

    Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,5 +1,6 @@
 def no_c(my_string):
-    for i in my_string:
-        if i=="c" and i=="C":
-            my_string.remove("i")
-    return(my_string)
+    space = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            space += char
+    return space
