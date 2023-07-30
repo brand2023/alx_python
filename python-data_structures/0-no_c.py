dbcb269c5e8290660f17e25c40e86191f65e9bc6
brand2023 <brandonestephane@gmail.com>
2023-07-31 00:07:30 +0200

    Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     chr=my_string
     for i in range(len(chr)):
-        if chr[i]=="c" or chr[i]=="C":
-            del chr[i]
+        if chr[i]=="c" and chr[i]=="C":
+            chr.remove(i)
     return(chr)
