c3aa2481d313204f591ffae79642ac136eac937e
brand2023 <brandonestephane@gmail.com>
2023-07-30 19:39:12 +0200

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,18 +1,11 @@
-#!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
+def main():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
 
-if __name__=="__main__":
-    a=1
-    b=2
-    from add_0 import add
-    print("{}+{}={}".format(a,b,add(a,b)), end="\n")
+if __name__ == "__main__":
+    main()
