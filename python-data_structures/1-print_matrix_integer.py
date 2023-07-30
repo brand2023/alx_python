2cd3376eecf45e2c592667d67e141cd8af410578
brand2023 <brandonestephane@gmail.com>
2023-07-31 01:00:41 +0200

    Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,3 +2,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i,k in enumerate(row):
             print("{:d}".format(k), end=" " if i<len(row)-1 else "")
+        print()
