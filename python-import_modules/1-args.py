1aa3aecd2228b555c5e2dda784aed3029ee685d7
brand2023 <brandonestephane@gmail.com>
2023-07-30 21:51:36 +0200

    Everything can be imported

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -15,6 +15,5 @@ def main():
         for i in range(1, num_arg + 1):
             print("{}: {}".format(i, sys.argv[i]))
 
-
 if __name__ == "__main__":
     main()
---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,5 @@
+from variable_load_2 import a
+def main():
+    print("a = {}".format(a))
+if __name__ == "__main__":
+    main()
