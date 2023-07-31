a943dc590b023bea2d7ec938842b96272b4de54e
brand2023 <brandonestephane@gmail.com>
2023-07-31 23:53:43 +0200

    Access and update private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,45 @@
+"""
+this is class
+
+"""
+
+
+class Square:
+    """
+    class Square that defines a square
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Instantiation with size (no type/value verification)
+
+        """
+        self.__size = size
+
+    def size(self):
+        """
+        Instantiation with size (no type/value verification)
+
+        """
+        return self.__size
+
+    def size(self, value):
+        """
+        Instantiation with size (no type/value verification)
+
+        """
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size
+
+    def area(self):
+        """
+        Instantiation with size (no type/value verification)
+
+        """
+        self.area = self.__size ** 2
+        return self.area
