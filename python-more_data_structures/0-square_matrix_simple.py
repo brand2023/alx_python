5bb70e7172a177d9a7f32d5c9e5dd1d91e19831c
brand2023 <brandonestephane@gmail.com>
2023-07-31 14:19:30 +0200

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 def square_matrix_simple(matrix=[]):
     for column in matrix:
         for i,k in enumerate(column):
-            print("{:d}".format(k*k),  end=" " if i<len(row)-1 else "")
+            print("{:d}".format(k*k),  end=" " if i<len(column)-1 else "")
         print()
         
