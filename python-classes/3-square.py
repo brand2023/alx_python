6ae6596e555029170b3e8b5a716e6be1ba158beb
brand2023 <brandonestephane@gmail.com>
2023-08-01 01:54:37 +0200

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -24,11 +24,12 @@ class Square:
         Instantiation with size (no type/value verification)
 
         """
-        if type(self.__size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def __init__(self, size=0):
         """
