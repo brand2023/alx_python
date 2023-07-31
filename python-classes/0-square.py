009e0180fdd94a9dd96c0606573d58d3b3173fb4
brand2023 <brandonestephane@gmail.com>
2023-07-31 21:33:20 +0200

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 class Square:
+    print(__import__("my_module").MyClass.__doc__)
     def Square(self,size):
+        print(__import__("my_module").my_function.__doc__)
+        print(__import__("my_module").MyClass.my_function.__doc__)
         self.__size=size
         return self.__size
