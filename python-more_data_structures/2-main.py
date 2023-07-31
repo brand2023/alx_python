bbb43219af699c9779264aa11f74a0e0a6cf62b6
brand2023 <brandonestephane@gmail.com>
2023-07-31 17:25:30 +0200

    Update dictionary

--- python-more_data_structures/2-main.py
+++ python-more_data_structures/2-main.py
@@ -6,20 +6,17 @@ def print_sorted_dictionary(my_dict):
     keys = sorted(my_dict.keys())
     for k in keys:
         print("{}: {}".format(k, my_dict[k]))
-def main():
-    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-    new_dict = update_dictionary(a_dictionary, 'language', "Python")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
 
-    print("--")
-    print("--")
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
 
-    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
-if __name__ == "__main__":
-    main()
+print("--")
+print("--")
 
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
