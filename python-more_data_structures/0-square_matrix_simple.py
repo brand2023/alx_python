45dde964d48e3c6190800f463e8aad4e3e3083fb
brand2023 <brandonestephane@gmail.com>
2023-07-31 13:49:58 +0200

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,6 @@
-def square_matrix_simple(matrix=[]):
-    matrix1=[]
-    matrix2=[]
-    for row in matrix1:
-        for i,k in enumerate(row):
-            print("{:d}".format(k), end=" " if i<len(row)-1 else "")
-        print()
-    for row in matrix2:
+def square_matrix_simple(matrix):
+    for row in matrix:
         for i,k in enumerate(row):
             print("{:d}".format(k*k),  end=" " if i<len(row)-1 else "")
         print()
-    matrix=list(map(matrix1, matrix2))
     return(matrix)
