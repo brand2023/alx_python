631926f6b46dc0313153766c410da197370edc85
brand2023 <brandonestephane@gmail.com>
2023-07-31 16:54:31 +0200

    Update dictionary

--- python-more_data_structures/2-main.py
+++ python-more_data_structures/2-main.py
@@ -3,9 +3,9 @@ update_dictionary = __import__('2-update_dictionary').update_dictionary
 
 def print_sorted_dictionary(my_dict):
     """ Print sorted dictionary """
-    keys = sorted(my_dict.keys)
-    for k in keys:
-        print("{}: {}".format(k, my_dict[k]))
+    keys = sorted(my_dict.keys())
+    for i in keys:
+        print("{}: {}".format(i,my_dict[i]))
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
