edd1d1d6b09d0a855d067c5ee606edd40326f96a
brand2023 <brandonestephane@gmail.com>
2023-07-31 21:59:11 +0200

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,18 @@
 """
-this is class 
+this is class
 
 """
 
 
 class Square:
-    """ 
+    """
     class Square that defines a square
 
     """
-    
-    def __init__(self,size):
+
+    def __init__(self, size):
         """
         Instantiation with size (no type/value verification)
 
         """
-        self.__size=size
+        self.__size = size
