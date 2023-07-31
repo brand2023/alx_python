911e8d6b59abe6d28fe69c70c4c39b05c7348cc9
brand2023 <brandonestephane@gmail.com>
2023-07-31 17:22:29 +0200

    Update dictionary

--- python-more_data_structures/2-main.py
+++ python-more_data_structures/2-main.py
@@ -4,19 +4,22 @@ update_dictionary = __import__('2-update_dictionary').update_dictionary
 def print_sorted_dictionary(my_dict):
     """ Print sorted dictionary """
     keys = sorted(my_dict.keys())
-    for i in keys:
-        print("{}: {}".format(i,my_dict[i]))
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
+def main():
+    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+    print("--")
+    print("--")
 
-print("--")
-print("--")
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+if __name__ == "__main__":
+    main()
 
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
