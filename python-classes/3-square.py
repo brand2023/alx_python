e0bb5b42f1a558ed2f0fa635fcd39451e89ade37
brand2023 <brandonestephane@gmail.com>
2023-08-01 00:43:30 +0200

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -16,10 +16,6 @@ class Square:
 
         """
         self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
