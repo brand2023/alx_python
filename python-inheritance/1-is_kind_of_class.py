01fe2bb6ece016fba5b65f49cd18e7f6a8f96676
brand2023 <brandonestephane@gmail.com>
2023-08-01 17:31:09 +0200

    Same class or inherit from

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,14 @@
+"""
+fonction that check instance of a class
+
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    the fonction
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
+ 
