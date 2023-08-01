6c831643924d2bb441914bbc97bbc37f16fb65b7
brand2023 <brandonestephane@gmail.com>
2023-08-01 16:26:16 +0200

    Printing a square

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -7,8 +7,7 @@ def is_same_class(obj, a_class):
     """
     the fonction
     """
-
-    if obj is isinstance(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
