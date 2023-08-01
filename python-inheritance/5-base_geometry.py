2f591136e165fbf4acf8fb2192da137e1f0b4642
brand2023 <brandonestephane@gmail.com>
2023-08-01 23:26:56 +0200

    Integer validator

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """area methode"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        """integer validator methode"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        elif value < 0:
+            raise ValueError("<name> must be greater than 0")
