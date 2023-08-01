dc78f54f836410b5441cc4d4f73e585a65583e86
brand2023 <brandonestephane@gmail.com>
2023-08-01 17:57:36 +0200

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -5,7 +5,7 @@ fonction that check instance of a class
 
 def is_kind_of_class(obj, a_class):
     """
-    the fonction
+    the fonction kind of class
     """
 
     if isinstance(obj, a_class) and not issubclass(a_class, type(obj)):
