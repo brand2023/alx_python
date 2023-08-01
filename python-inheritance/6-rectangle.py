0e9c691ddec3444e3dadf45ce00fdf7cb016c38c
brand2023 <brandonestephane@gmail.com>
2023-08-02 00:14:05 +0200

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,19 +1,8 @@
-"""Geometry module"""
+"""Rectangle class"""
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
-    def area(self):
-        """area methode"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer validator methode"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+class Rectangle:
+    """REc class"""
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
