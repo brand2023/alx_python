3c6294995b178abfaf9f2e4b82c9316babf76b1c
brand2023 <brandonestephane@gmail.com>
2023-08-02 20:51:49 +0200

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,5 +4,11 @@
 
 class BaseGeometry:
     """BaseGeometry class"""
-    def __dir__(BaseGeometry) -> None:
-        pass
+    def __dir__(cls) -> None:
+        Attributes=super().__dir__()
+        liste_to_return=[]
+        for attr in Attributes:
+            if attr != "__init_subclass__":
+                liste_to_return.append(attr)
+        return liste_to_return
+    pass
