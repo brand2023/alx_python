2f1dc3e1581e9154c717d15da431affc1bb22ad4
brand2023 <brandonestephane@gmail.com>
2023-08-04 03:10:47 +0200

    Full rectangle

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,3 +8,9 @@ class BaseGeometry():
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
     pass
+
+class Myclass(metaclass = BaseGeometry):
+    """BaseGeometry class"""
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
