83c9b089d304b6bb45bdbebdd756fc72259e0b21
brand2023 <brandonestephane@gmail.com>
2023-08-04 13:44:44 +0200

    Full rectangle

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,3 +8,16 @@ class BaseGeometry():
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
     pass
+
+class BrandMetaClass(type):
+    """
+    documentation
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BrandClass(metaclass = BrandMetaClass):
+    """documentation for class BrandClass"""
+
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
