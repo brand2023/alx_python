3d03332ab9596668bf6512cb1c75053669dff116
brand2023 <brandonestephane@gmail.com>
2023-08-06 00:17:49 +0200

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,28 @@
+Base = __import__('base').Base
+
+"""Module documentatin"""
+
+
+class Rectangle(Base):
+    """class documentation"""
+    @property
+    def attributes(self):
+         """
+        documentation of def
+
+        """
+         return self.__width, self.__height, self.__x, self.__y
+
+    @attributes.setter
+    def attributes(self, value1, value2, value3, value4):
+         self.__width = value1
+         self.__height = value2
+         self.__x = value3
+         self.__y = value4
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+         id = super().__init__()
+         self.__width = width
+         self.__height = height
+         self.__x = x
+         self.__y = y
