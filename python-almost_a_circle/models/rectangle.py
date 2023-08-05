7e1554c30be551f5b4212d7ac99541d069a45eac
brand2023 <brandonestephane@gmail.com>
2023-08-06 00:35:01 +0200

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,33 +1,33 @@
 """Module Base documentatin"""
-Base = __import__('base').Base
+Base = __import__('models/base').Base
 
 
 class Rectangle(Base):
     """class base documentation"""
     @property
     def attributes(self):
-         """
+        """
         documentation of def
 
         """
-         return self.__width, self.__height, self.__x, self.__y
+        return self.__width, self.__height, self.__x, self.__y
 
     @attributes.setter
     def attributes(self, value1, value2, value3, value4):
-         """documentation setter"""
-         self.__width = value1
-         self.__height = value2
-         self.__x = value3
-         self.__y = value4
+        """documentation setter"""
+        self.__width = value1
+        self.__height = value2
+        self.__x = value3
+        self.__y = value4
 
     def __init__(self, width, height, x=0, y=0, id=None):
-            """Initializes an instance.
-            Args:
-            - width: width of the rectangle
-            - heigth: height of the rectangle
-            """
-            id = super().__init__()
-            self.__width = width
-            self.__height = height
-            self.__x = x
-            self.__y = y
+        """Initializes an instance.
+        Args:
+        - width: width of the rectangle
+        - heigth: height of the rectangle
+        """
+        id = super().__init__()
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
