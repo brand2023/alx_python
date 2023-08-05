6617f463e0114e9c24627987303de1ab58ab4814
brand2023 <brandonestephane@gmail.com>
2023-08-05 23:33:55 +0200

    Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,12 @@
+"""Module documentatin"""
+
+
+
 class Base:
+    """class documentation"""
     __nb_objects = 0
     def __init__(self, id=None):
+        """methode documentation"""
         if id != None:
             self.id = id
         else:
