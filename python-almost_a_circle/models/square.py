faae5fc664658bab78f2b3e44716342dc76b3e02
brand2023 <brandonestephane@gmail.com>
2023-08-07 00:19:45 +0200

    And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,12 +6,10 @@ class Square(Rectangle):
     """class square documentation"""
     def __init__(self, size, x=0, y=0, id=None):
         """methode documentation"""
-        super().__init__(self.__width, self.__height, self.__x, self.__y, self.id)
-        self.size = self.__width = self.__height
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """overriding the __str__ method ."""
-        super().__init__(self.__width, self.__height, self.__x, self.__y, self.id)
         string = "[Square] ({})".format(self.id)
         string += " {}/{}".format(self.__x, self.__y)
         string += " - {}".format(self.size)
