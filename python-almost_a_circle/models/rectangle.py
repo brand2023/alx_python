6ec6d28b22db50f6d526e147086f0c80d4463e90
brand2023 <brandonestephane@gmail.com>
2023-08-06 20:06:11 +0200

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -40,36 +40,36 @@ class Rectangle(Base):
     def width(self, value1):
         """documentation setter"""
         if not isinstance(value1, int):
-            raise TypeError ("width must be an integer")
+            raise TypeError("width must be an integer")
         if value1 <= 0:
-            raise ValueError ("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value1
 
     @height.setter
     def height(self, value2):
         """documentation setter"""
         if not isinstance(value2, int):
-            raise TypeError ("height must be an integer")
+            raise TypeError("height must be an integer")
         if value2 <= 0:
-            raise ValueError ("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value2
 
     @x.setter
     def x(self, value3):
         """documentation setter"""
         if not isinstance(value3, int):
-            raise TypeError ("x must be an integer")
+            raise TypeError("x must be an integer")
         if value3 < 0:
-            raise ValueError ("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value3
 
     @y.setter
     def y(self, value4):
         """documentation setter"""
         if not isinstance(value4, int):
-            raise TypeError ("y must be an integer")
+            raise TypeError("y must be an integer")
         if value4 < 0:
-            raise ValueError ("y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value4
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -83,3 +83,19 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
