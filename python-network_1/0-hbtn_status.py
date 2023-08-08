0fc061d6bd9b9cbb1af2d37615d97d20575ab81b
brand2023 <brandonestephane@gmail.com>
2023-08-09 01:41:36 +0200

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,3 +2,10 @@
 
 import requests
 req = requests.get("https://alu-intranet.hbtn.io/status")
+content = str(req.content, "UTF-8")
+
+reponse = """
+Body response:
+\t- type: {}
+\t- content: {}
+""".format(type(content), content)
