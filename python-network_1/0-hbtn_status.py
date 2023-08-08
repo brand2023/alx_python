d1dd2600bc8025d02d346f4736db27141a3865be
brand2023 <brandonestephane@gmail.com>
2023-08-09 01:50:30 +0200

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 
 import requests
 if __name__ == "__main__":
-    req = requests.get("https://alu-intranet.hbtn.io/status")
+    req = get("https://alu-intranet.hbtn.io/status")
     content = str(req.content, "UTF-8")
 
     reponse = """
-    Body response:
-    \t- type: {}
-    \t- content: {}
-    """.strip().stripformat(type(content), content)
+Body response:
+\t- type: {}
+\t- content: {}
+""".strip().stripformat(type(content), content)
     print(reponse)
