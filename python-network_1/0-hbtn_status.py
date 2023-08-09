966988c9538a7e791d05902fa2818c9d3e2a753c
brand2023 <brandonestephane@gmail.com>
2023-08-09 16:19:32 +0200

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 """documentation module"""
 
 from requests import get
-if __name__ == "__main__":
-    req = get("https://alu-intranet.hbtn.io/status")
-    content = str(req.content, "UTF-8")
 
-    reponse = """
+req = get("https://alu-intranet.hbtn.io/status")
+content = str(req.content, "UTF-8")
+
+reponse = """
 Body response:
 \t- type: {}
 \t- content: {}
 """.strip().stripformat(type(content), content)
-    print(reponse)
+print(reponse)
