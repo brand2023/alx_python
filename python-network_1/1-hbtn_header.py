a25cb924404ce2a16df70dd3339a7f128f1e22e9
brand2023 <brandonestephane@gmail.com>
2023-08-10 22:41:44 +0200

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ def __main__():
     """documentation main fonction"""
     url = s.argv[1]
     req = r.get(url)
-    header = req.headers['X-Request-Id']
+    header = req.headers.get('X-Request-Id')
     return header
 if __name__ == "__main__":
     __main__
