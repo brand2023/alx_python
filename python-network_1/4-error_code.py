b87ca49b94f4dae05a68540ec9d6ef6af07f117b
brand2023 <brandonestephane@gmail.com>
2023-08-10 23:51:43 +0200

    Error code #1

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,10 @@
+"""documentation module"""
+import requests as r
+import sys as s
+
+
+if __name__ == "__main__":
+    response = r.get(s.argv[1])
+    code = response.status_code
+    if code >= 400:
+        print("Error code: ",code)
