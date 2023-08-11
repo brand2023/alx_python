1372e47639907674d51d37429fda11a5b9ed401a
brand2023 <brandonestephane@gmail.com>
2023-08-11 03:13:12 +0200

    My GitHub!

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -5,8 +5,8 @@ import sys as s
 
 if __name__ == "__main__":
 
-    response = r.get("https://api.github.com/user")
     username = s.argv[1]
     password = s.argv[2]
+    response = r.get("https://api.github.com/user/{username}")
     jeson = response.json()
     print (jeson["id"])
