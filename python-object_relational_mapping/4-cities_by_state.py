e1575f154da55902ae3d93cd869fc675b421f3f6
brand2023 <brandonestephane@gmail.com>
2023-09-23 15:17:22 +0200

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,13 @@ import sys
 def cities():
     """def states"""
     
-    db_connect = Mydb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db_connect = Mydb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
     db_cursor = db_connect.cursor()
     db_cursor.execute("SELECT * FROM cities")
     rows_selected = db_cursor.fetchall()
