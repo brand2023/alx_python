76975f4d8a2ad0773fcd82423d12c2437dc02417
brand2023 <brandonestephane@gmail.com>
2023-09-23 14:39:47 +0200

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,12 +3,21 @@
 import MySQLdb as Mydb
 import sys
 
+
 def states():
     """def states"""
-    
-    db_connect = Mydb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    db_connect = Mydb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4]))
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE %s", (sys.argv[4])
+        )
     rows_selected = db_cursor.fetchall()
     for row in rows_selected:
         print(row)
