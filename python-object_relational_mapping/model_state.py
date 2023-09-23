3593f57885bb313fe2676418b00ad7282f03e26b
brand2023 <brandonestephane@gmail.com>
2023-09-23 16:38:41 +0200

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,9 +6,8 @@ mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 
-"""class definition"""
-
 class State(Base):
+        """class definition"""
         __tablename__ = "states"
         id = Column(Integer, unique=True, nullable=False, primary_key=True)
         name = Column(String(128), nullable=False)
