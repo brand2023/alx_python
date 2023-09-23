a6730f37a18fc65201185ebef67fe761e4a86a11
brand2023 <brandonestephane@gmail.com>
2023-09-23 14:01:16 +0200

    Python - Object-relational mapping

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+"""importing MySQLdb"""
+
+import MySQLdb as Mydb
+import sys
+
+def states():
+    """def states"""
+
+    db_connect = Mydb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY states.id")
+    rows_selected = db_cursor.fetchall()
+    for row in rows_selected:
+        print(row)
+if __name__ == "__main__":
+    states()
