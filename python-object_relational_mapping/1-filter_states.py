48e2b642f2bd70e476ba9e910f5d7edcf8c6c279
brand2023 <brandonestephane@gmail.com>
2023-09-23 14:19:31 +0200

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ def states():
     """def states"""
 
     db_connect = Mydb.connect(
-        host="localhost", port=3306, 
-        user=sys.argv[1], 
+        host="localhost", port=3306,
+        user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
         )
