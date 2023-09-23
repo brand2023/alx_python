51fa8383fa68d69208783d797a63596ca42aa1e9
brand2023 <brandonestephane@gmail.com>
2023-09-23 17:23:07 +0200

    HBNB

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,14 +1,15 @@
+"""flask importing"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def Hello_HBNB():
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
-    return "<p>HBNB</p>"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 5000)
